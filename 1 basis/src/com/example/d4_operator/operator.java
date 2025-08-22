466b0c042aca296241c07670e3ca0195f730fb23
FancyXGit <Fancy.fantasy688@gmail.com>
2025-08-22 14:46:17 +0800

    Add Repository

---
+++ 1 basis/src/com/example/d4_operator/operator.java
@@ -0,0 +1,15 @@
+package com.example.d4_operator;
+
+public class operator {
+    public static void main(String[] args){
+        int a = 0;
+        int b = 1;
+        b += a;
+        a++;
+        System.out.println(a);
+        System.out.println(b);
+        Boolean flag1 = true;
+        Boolean flag2 = false;
+        System.out.println(flag2 && flag1);
+    }
+}
